82770cb0a376bbc126acbf49fbceb030dc230d97
Andrew McNeill <mcneill.andrew.d@gmail.com>
2014-11-18 12:01:32 -0600

    added more tube letters, fixed compile issue

--- .ino.cpp
+++ .ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2014-11-04 17:08:26
+//This file has been generated on 2014-11-18 11:52:56
 
 #include "Arduino.h"
 #include <Arduino.h>
@@ -13,7 +13,6 @@ float calcExposureTime(float EV) ;
 uint8_t calcSSIndex(float exposureTime) ;
 void checkButtons() ;
 void updateNextTube() ;
-void serialShowTubes() ;
 void initiatePins() ;
 void setup() ;
 void loop() ;
--- CameraProject.ino
+++ CameraProject.ino
@@ -3,21 +3,27 @@
 // Define pins
 #define LIGHT_METER_PIN		A4
 #define SELECT_BUTTON_PIN	1
-#define BACK_BUTTON_PIN		A5				// TODO: determine if i need a back button
+#define BACK_BUTTON_PIN		A5
 #define UP_BUTTON_PIN		11
 #define DOWN_BUTTON_PIN		12
 #define SHUTTER_BUTTON_PIN	0
 #define SHUTTER_CONTROL_PIN 13
+#define DOT_PIN				9
+#define LED_PIN				10
 
 // Define needed constants
 #define F_STOP				11.375			// f/ of camera 182mm FL / 16mm aperture
 #define SS_ARRAY_SIZE		46
+
+// Define tube letters
 #define TUBE_OFF			10
+#define TUBE_O				0
+#define TUBE_S				5
+#define TUBE_T				7
+#define TUBE_G				9
 #define TUBE_E				11
 #define TUBE_C				12
 #define TUBE_v				13
-#define DOT_PIN				9
-#define LED_PIN				10
 
 // Define tube stuff
 /*
@@ -37,19 +43,19 @@
        pin 5
 */
 
-static const byte tubeChar[14*7] PROGMEM =
+static byte tubeChar[14*7] PROGMEM =
 {
 // pin    2     3     4     5     6     7     8
-        HIGH, HIGH, HIGH, HIGH, HIGH, HIGH,  LOW,	// 0
+        HIGH, HIGH, HIGH, HIGH, HIGH, HIGH,  LOW,	// 0/TUBE_O
          LOW, HIGH, HIGH,  LOW,  LOW,  LOW,  LOW,	// 1
         HIGH, HIGH,  LOW, HIGH, HIGH,  LOW, HIGH,	// 2
         HIGH, HIGH, HIGH, HIGH,  LOW,  LOW, HIGH,	// 3
          LOW, HIGH, HIGH,  LOW,  LOW, HIGH, HIGH,	// 4
-        HIGH,  LOW, HIGH, HIGH,  LOW, HIGH, HIGH,	// 5
+        HIGH,  LOW, HIGH, HIGH,  LOW, HIGH, HIGH,	// 5/TUBE_S
         HIGH,  LOW, HIGH, HIGH, HIGH, HIGH, HIGH,	// 6
-        HIGH, HIGH, HIGH,  LOW,  LOW,  LOW,  LOW,	// 7
+        HIGH, HIGH, HIGH,  LOW,  LOW,  LOW,  LOW,	// 7/TUBE_T
         HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH,	// 8
-        HIGH, HIGH, HIGH, HIGH,  LOW, HIGH, HIGH,	// 9
+        HIGH, HIGH, HIGH, HIGH,  LOW, HIGH, HIGH,	// 9/TUBE_G
          LOW,  LOW,  LOW,  LOW,  LOW,  LOW,  LOW,  	// TUBE_OFF
         HIGH,  LOW,  LOW, HIGH, HIGH, HIGH, HIGH,	// TUBE_E
         HIGH,  LOW,  LOW, HIGH, HIGH, HIGH,  LOW,	// TUBE_C
@@ -133,7 +139,7 @@ void checkButtons()
 
 void updateNextTube()
 {
-	byte   *digit_seg_p;
+	byte    *digit_seg_p;
 	byte    digit;
 	byte    pin;
 
@@ -142,7 +148,7 @@ void updateNextTube()
 	currentTube %= 4;									// roll over if needed
 
 	digit = tubeData[currentTube].digit;
-	digit_seg_p = tubeChar + 7*digit;					// TODO: i think this line got fucked up
+	digit_seg_p = tubeChar + 7*digit;
 	for (pin = 2; pin < 9; pin++, digit_seg_p++) 		// light needed segments
 	{
 		digitalWrite(pin,pgm_read_byte(digit_seg_p));
@@ -150,11 +156,6 @@ void updateNextTube()
 	digitalWrite(DOT_PIN,tubeData[currentTube].dot); 	// turn dot on or off
 	digitalWrite(LED_PIN,tubeData[currentTube].led);	// turn the led on of off
 	digitalWrite(A0+currentTube,HIGH);					// enable the current tube
-}
-
-void serialShowTubes()
-{
-
 }
 
 void initiatePins()
@@ -191,10 +192,8 @@ void setup()
 void loop()
 {
 	checkButtons();
-
-	serialShowTubes();	// enable this for debugging
-	//updateNextTube();	 // enable this when tubes are actuall connected
-	delay(2);		// Delay 2 ms => at most 500 cycles/second
+	updateNextTube();	 			// enable this when tubes are actuall connected
+	delay(2);			 			// Delay 2 ms => at most 500 cycles/second
 }
 
 // testing code
