5e1a3daebc3585898480d390ef9dd2b9f3d264be
Andrew McNeill <mcneill.andrew.d@gmail.com>
2014-11-20 09:41:01 -0600

    tested, working

--- .ino.cpp
+++ .ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2014-11-19 17:03:09
+//This file has been generated on 2014-11-19 22:51:59
 
 #include "Arduino.h"
 #include <Arduino.h>
